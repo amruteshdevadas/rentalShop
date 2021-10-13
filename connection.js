59bf718ad99482b38c0e8bdcdc497e3f3430872d
Amrutesh Devadas <amruteshdevadas@gmail.com>
2021-10-13 21:30:35 +0530

    mongo_passwoed_changed

--- connection.js
+++ connection.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 exports.connect = ()=>{
     try {
         const mangoose =require("mongoose")
         mangoose.connect(
             // `mongodb://localhost:27017/rentalShop`
-            "mongodb+srv://amrutesh:amrutesh@123@cluster0.yj2ao.mongodb.net?retryWrites=true&w=majority",
+            `mongodb+srv://amrutesh:${process.env.MONGO_PASS}@cluster0.yj2ao.mongodb.net?retryWrites=true&w=majority`,
 
             {useNewUrlParser:true,useUnifiedTopology:true})
         // mangoose.set('useFindandModify',false)

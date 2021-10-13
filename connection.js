3e005fb29b553044bc784162867981b2378a30f8
Amrutesh Devadas <amruteshdevadas@gmail.com>
2021-10-13 22:55:26 +0530

    mongo_atlas_connected

--- connection.js
+++ connection.js
@@ -4,10 +4,15 @@ exports.connect = ()=>{
         const mangoose =require("mongoose")
         mangoose.connect(
             // `mongodb://localhost:27017/rentalShop`
-            `mongodb+srv://amrutesh:${process.env.MONGO_PASS}@cluster0.yj2ao.mongodb.net/rentalShop?retryWrites=true&w=majority`,
+            `mongodb+srv://amrutesh:${process.env.MONGO_PASS}.yj2ao.mongodb.net/rentalShop?retryWrites=true&w=majority`,
 
             {useNewUrlParser:true,useUnifiedTopology:true})
         // mangoose.set('useFindandModify',false)
+        mangoose.connection.once("open",()=>{console.log("connected")})
+        .on("error",error =>{
+            console.log("Your Error",error)
+        })
+
         
     } catch (error) {
         console.log(error)

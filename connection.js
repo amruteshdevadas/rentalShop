985b59208bd984a3845548385328f8ae8189c003
Amrutesh Devadas <amruteshdevadas@gmail.com>
2021-10-13 21:26:53 +0530

    mongo_link_added

--- connection.js
+++ connection.js
@@ -1,7 +1,11 @@
 exports.connect = ()=>{
     try {
         const mangoose =require("mongoose")
-        mangoose.connect(`mongodb://localhost:27017/rentalShop`,{useNewUrlParser:true,useUnifiedTopology:true})
+        mangoose.connect(
+            // `mongodb://localhost:27017/rentalShop`
+            "mongodb+srv://amrutesh:amrutesh@123@cluster0.yj2ao.mongodb.net?retryWrites=true&w=majority",
+
+            {useNewUrlParser:true,useUnifiedTopology:true})
         // mangoose.set('useFindandModify',false)
         
     } catch (error) {
